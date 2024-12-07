5aac238679e7705c4e249fc360f1a4a7a3edb555
rimasultana <rimasultana20225@gmail.com>
2024-12-07 23:14:26 +0600

    vercel update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const morgan = require("morgan");
+require("dotenv").config();
 const app = express();
 const port = 5000;
 
@@ -9,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-const uri =
-  "mongodb+srv://movie-side:Teznr2ewfpP6UjgS@cluster0.4hbah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hbah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
